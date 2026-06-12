823bf55a0be7a0ce1c8fdd7027073a23fc1ebd87
Marianna Martindale <marianna.martindale@gmail.com>
2026-06-11 23:09:57 -0400

    adding config.js for real this time

---
+++ config.js
@@ -0,0 +1,4 @@
+// Copy this file to config.js and fill in your values.
+// config.js is gitignored — never commit it.
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxjOs6IbzeZ80PSOYVWjps6EY_sS3wIBsgmGteYnf12-uHqcuPEXp-4lu35EO8Z112M/exec";
+const SHEET_ID = "1d1a0qDXaGnGkPdOC9eWfsSpWeVWDdR-vBLT0N_8qHQM";
